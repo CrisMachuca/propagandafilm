e53d953d9b56f7ed9412e3d4ff357493e6a8e0e0
CrisMachuca <cristinamachucam@gmail.com>
2023-05-17 02:04:00 +0200

    agrego cookies

--- js/main.js
+++ js/main.js
@@ -52,3 +52,124 @@ function closeSubmenus() {
   });
 }
 */
+
+// Función para establecer una cookie con una duración de días específica
+function setCookie(name, value, days) {
+  var expires = "";
+  if (days) {
+    var date = new Date();
+    date.setTime(date.getTime() + (days * 24 * 60 * 60 * 1000));
+    expires = "; expires=" + date.toUTCString();
+  }
+  document.cookie = name + "=" + (value || "") + expires + "; path=/";
+}
+
+// Función para obtener el valor de una cookie específica
+function getCookie(name) {
+  var nameEQ = name + "=";
+  var ca = document.cookie.split(';');
+  for (var i = 0; i < ca.length; i++) {
+    var c = ca[i];
+    while (c.charAt(0) == ' ') c = c.substring(1, c.length);
+    if (c.indexOf(nameEQ) == 0) return c.substring(nameEQ.length, c.length);
+  }
+  return null;
+}
+
+// Función para eliminar una cookie específica
+function eraseCookie(name) {
+  document.cookie = name + '=; Max-Age=-99999999;';
+}
+
+// Función para mostrar u ocultar el aviso de cookies
+function toggleCookieBanner() {
+  var cookieBanner = document.getElementById("cookie-banner");
+  cookieBanner.style.display = (getCookie("cookieConsent") === "true") ? "none" : "block";
+}
+
+// Función para aceptar el uso de cookies
+function acceptCookies() {
+  setCookie("cookieConsent", "true", 30); // Establece la cookie por 30 días
+  toggleCookieBanner();
+}
+
+// Función para rechazar el uso de cookies
+function rejectCookies() {
+  setCookie("cookieConsent", "false", 30); // Establece la cookie por 30 días
+  toggleCookieBanner();
+}
+
+// Función para gestionar las preferencias de cookies
+function manageCookies() {
+  // Aquí puedes redirigir al usuario a una página de gestión de cookies o abrir un modal para configurar las preferencias
+  console.log("Redireccionando a la página de gestión de cookies...");
+}
+
+// Función para cerrar el aviso de cookies
+function closeCookieBanner() {
+  var cookieBanner = document.getElementById("cookie-banner");
+  cookieBanner.style.display = "none";
+}
+
+// Función para aceptar el uso de cookies
+function acceptCookies() {
+  setCookie("cookieConsent", "true", 30); // Establece la cookie por 30 días
+  closeCookieBanner();
+}
+
+// Agregar controlador de eventos al botón de aceptar
+var acceptButton = document.getElementById("accept-button");
+acceptButton.addEventListener("click", acceptCookies);
+
+// Función para rechazar el uso de cookies
+function rejectCookies() {
+  setCookie("cookieConsent", "false", 30); // Establece la cookie por 30 días
+  closeCookieBanner();
+}
+
+// Agregar controlador de eventos al botón de rechazar
+var rejectButton = document.getElementById("reject-button");
+rejectButton.addEventListener("click", rejectCookies);
+
+
+
+
+// Función para mostrar el modal de gestión de cookies
+function showCookieModal() {
+  var cookieModal = document.getElementById("cookie-modal");
+  cookieModal.style.display = "block";
+}
+
+// Función para cerrar el modal de gestión de cookies
+function closeCookieModal() {
+  var cookieModal = document.getElementById("cookie-modal");
+  cookieModal.style.display = "none";
+}
+
+// Función para guardar la opción de cookies seleccionada por el usuario
+function saveCookieOption() {
+  var cookieOption = document.querySelector('input[name="cookie-option"]:checked').value;
+  setCookie("cookieOption", cookieOption, 30); // Establece la cookie por 30 días
+  closeCookieModal();
+}
+
+// Agregar controlador de eventos al botón de gestionar
+var manageButton = document.getElementById("manage-button");
+manageButton.addEventListener("click", showCookieModal);
+
+// Agregar controlador de eventos al botón de cerrar del modal
+var closeButton = document.getElementById("close-button");
+closeButton.addEventListener("click", closeCookieModal);
+
+// Agregar controlador de eventos al botón de guardar del modal
+var saveButton = document.getElementById("save-button");
+saveButton.addEventListener("click", saveCookieOption);
+
+
+// JavaScript para cerrar el banner de cookies al hacer clic en el botón de cierre
+const closeBtn = document.querySelector('.close-btn');
+const cookieBanner = document.querySelector('#cookie-banner');
+
+closeBtn.addEventListener('click', function() {
+  cookieBanner.style.display = 'none';
+});
