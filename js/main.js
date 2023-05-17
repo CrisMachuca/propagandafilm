1eda00f803f2452c5e175978640d041fb09d9e2f
CrisMachuca <cristinamachucam@gmail.com>
2023-05-17 12:22:36 +0200

    optimizacion de imagenes

--- js/main.js
+++ js/main.js
@@ -25,33 +25,6 @@ select.addEventListener('click', () => {
 
 
 
-/*
-
-const menuItems = document.querySelectorAll('.menu li.dropdown');
-menuItems.forEach(item => {
-  const submenu = item.querySelector('.dropdown-content');
-  const link = item.querySelector('a');
-  link.addEventListener('click', (e) => {
-    e.preventDefault();
-    if (submenu.classList.contains('submenu-open')) {
-      submenu.classList.remove('submenu-open');
-      submenu.style.maxHeight = null;
-    } else {
-      closeSubmenus();
-      submenu.classList.add('submenu-open');
-      submenu.style.maxHeight = submenu.scrollHeight + 'px';
-    }
-  });
-});
-
-function closeSubmenus() {
-  const submenus = document.querySelectorAll('.dropdowncontent.submenu-open');
-  submenus.forEach(submenu => {
-    submenu.classList.remove('submenu-open');
-    submenu.style.maxHeight = null;
-  });
-}
-*/
 
 // Función para establecer una cookie con una duración de días específica
 function setCookie(name, value, days) {
